ca70f8583717acfef0fb8bd18d73c837c322702a
Ams <streetw@yahoo.com>
2022-12-20 01:52:43 -0500

    Routing added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import Header from "./shared/Header";
 import styles from "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <Header />
+        {/* <Home /> */}
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
